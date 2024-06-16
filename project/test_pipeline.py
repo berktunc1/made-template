03886b68b57dcc804a9ea077efaf4faa4dd0a0c7
berktunc1 <167681046+berktunc1@users.noreply.github.com>
2024-06-17 00:11:03 +0300

    Update test_pipeline.py

--- project/test_pipeline.py
+++ project/test_pipeline.py
@@ -1,26 +1,26 @@
 import unittest
 import subprocess
-import io
-import sys
 import os
 
 class TestPipeline(unittest.TestCase):
 
     def test_pipeline(self):
-        captured_output = io.StringIO()
-        sys.stdout = captured_output
-
+        # Run the pipeline script
         script_path = os.path.join(os.path.dirname(__file__), "pipeline.py")
         result = subprocess.run(["python", script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        sys.stdout = sys.__stdout__
 
-        # Get the output and check for specific print statements
+        # Capture stdout and stderr
         output = result.stdout
-        print("Captured Output:\n", output)
+        errors = result.stderr
 
+        print("Captured Output:\n", output)
+        print("Captured Errors:\n", errors)
+
+        # Check if the output contains the expected print statement for CO2 data
         self.assertIn("Data pipeline executed successfully. Cleaned data stored at ../data/co2_emissions.sqlite", output,
                       "The CO2 emissions success message was not found in the output.")
 
+        # Check if the output contains the expected print statement for Population data
         self.assertIn("Data pipeline executed successfully. Cleaned data stored at ../data/population.sqlite", output,
                       "The Population success message was not found in the output.")
 
