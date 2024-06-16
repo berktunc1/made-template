6391f5c7535ef4c0bd9a1331ba1a0bbe8af18009
berktunc1 <167681046+berktunc1@users.noreply.github.com>
2024-06-17 00:22:02 +0300

    Update test_pipeline.py

--- project/test_pipeline.py
+++ project/test_pipeline.py
@@ -5,7 +5,7 @@ import os
 class TestPipeline(unittest.TestCase):
 
     def test_pipeline(self):
-        # Run the pipeline script
+        # Run the pipeline script with the use_mock parameter
         script_path = os.path.join(os.path.dirname(__file__), "pipeline.py")
         result = subprocess.run(["python", script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
@@ -16,13 +16,9 @@ class TestPipeline(unittest.TestCase):
         print("Captured Output:\n", output)
         print("Captured Errors:\n", errors)
 
-        # Check if the output contains the expected print statement for CO2 data
-        self.assertIn("Data pipeline executed successfully. Cleaned data stored at ../data/co2_emissions.sqlite", output,
-                      "The CO2 emissions success message was not found in the output.")
-
-        # Check if the output contains the expected print statement for Population data
-        self.assertIn("Data pipeline executed successfully. Cleaned data stored at ../data/population.sqlite", output,
-                      "The Population success message was not found in the output.")
+        # Check if the output contains the expected print statements
+        self.assertIn("Data pipeline executed successfully. Cleaned data stored at :memory:", output,
+                      "The success message was not found in the output.")
 
 if __name__ == "__main__":
     unittest.main()
